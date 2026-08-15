3f4bb5bd30ed9e50f9ca2e188330228a2fa1ac24
zummorow <cucakrowo38@gmail.com>
2026-08-15 23:01:17 +0700

    feat: add MedalStanding type and initial medal standings data array

--- src/data/medalStandings.ts
+++ src/data/medalStandings.ts
@@ -7,9 +7,9 @@ export type MedalStanding = {
 };
 
 export const medalStandings: MedalStanding[] = [
-  { id: 1, name: 'Sekretariat Jenderal', gold: 0, silver: 5, bronze: 2 },
+  { id: 1, name: 'Sekretariat Jenderal', gold: 1, silver: 5, bronze: 2 },
   { id: 2, name: 'Inspektorat Jenderal', gold: 2, silver: 0, bronze: 2 },
-  { id: 3, name: 'Direktorat Jenderal Kesehatan Primer Komunitas', gold: 4, silver: 1, bronze: 2 },
+  { id: 3, name: 'Direktorat Jenderal Kesehatan Primer Komunitas', gold: 3, silver: 1, bronze: 2 },
   { id: 4, name: 'Direktorat Jenderal Kesehatan Lanjutan', gold: 1, silver: 3, bronze: 1 },
   { id: 5, name: 'Direktorat Jenderal Penanggulangan Penyakit', gold: 4, silver: 0, bronze: 4 },
   { id: 6, name: 'Direktorat Jenderal Farmasi dan Alat Kesehatan', gold: 0, silver: 1, bronze: 1 },
