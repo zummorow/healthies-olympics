275dd5c0add5681edf12fcb0825426a48a702ced
zummorow <cucakrowo38@gmail.com>
2026-08-16 09:58:53 +0700

    feat: add medalStandings data structure and initial dataset

--- src/data/medalStandings.ts
+++ src/data/medalStandings.ts
@@ -11,7 +11,7 @@ export const medalStandings: MedalStanding[] = [
   { id: 2, name: 'Inspektorat Jenderal', gold: 2, silver: 0, bronze: 2 },
   { id: 3, name: 'Direktorat Jenderal Kesehatan Primer Komunitas', gold: 3, silver: 1, bronze: 2 },
   { id: 4, name: 'Direktorat Jenderal Kesehatan Lanjutan', gold: 1, silver: 3, bronze: 1 },
-  { id: 5, name: 'Direktorat Jenderal Penanggulangan Penyakit', gold: 4, silver: 1, bronze: 4 },
+  { id: 5, name: 'Direktorat Jenderal Penanggulangan Penyakit', gold: 4, silver: 1, bronze: 3 },
   { id: 6, name: 'Direktorat Jenderal Farmasi dan Alat Kesehatan', gold: 0, silver: 1, bronze: 1 },
   { id: 7, name: 'Direktorat Jenderal Sumber Daya Manusia Kesehatan', gold: 1, silver: 3, bronze: 0 },
   { id: 8, name: 'Badan Kebijakan Pembangunan Kesehatan', gold: 2, silver: 0, bronze: 2 },
